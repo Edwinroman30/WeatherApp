a66381ca0b99aac643a32c69d94e4fb75642f0b7
Edwin Roman <romanseberino@gmail.com>
2022-01-10 01:22:31 -0400

    Adding more sense to menu
    I have tried to redefine the menu including more things.

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const {menuInquirer, pausaInquirer} = require('./helpers/inquirer'); 
+const {menuInquirer, pausaInquirer, questioning} = require('./helpers/inquirer'); 
+const CBusquedas = require('./models/CBusquedas');
 
 const Main = async () =>{
 
     let option;
+    const busqueda = new CBusquedas();
 
     do{
         console.clear();
         option = await menuInquirer();
+
+        switch(option){
+            
+            case 1:
+
+                //Search for city/place.
+                const {answare} = await questioning('Type the city you wish: ');
+
+                //Choose the right/ place you wish.
+                const resp = await busqueda.searchPlace( answare );
+                //console.log(`\n  ${resp}  \n`);
+
+                //Temperature
+
+                //Show the information about it:
+                console.log(`\nInformation about ${answare}\n`);
+                console.log(`City name: {}`);
+                console.log(`Long: {}`);
+                console.log(`Lat: {}`);
+                console.log(`Temperature: {}`);
+                console.log(`Max: {}`);
+                console.log(`Min: {}`);
+                
+                break;
+
+            case 2:
+                break;
+        }
 
         if(option !== 0)
             await pausaInquirer();
